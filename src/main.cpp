eb26963763a6e38840e657581ace60e08602ceae
Oliver Esser <oliver.esser@campus.tu-berlin.de>
2019-10-31 14:54:21 +0100

    Parse dot file in main

--- src/main.cpp
+++ src/main.cpp
@@ -4,74 +4,56 @@
 #include "FindCriticalPath.hpp"
 #include "FindRoots.hpp"
 #include "Graph.hpp"
+#include "ParseDotGraph.hpp"
 #include "TopologicalSort.hpp"
 #include "WeightedGraph.hpp"
 
-auto operator<<(std::ostream &out, const NodeList &nodes) -> std::ostream & {
-  std::copy(begin(nodes), end(nodes),
-            std::ostream_iterator<NodeIndex>{out, " "});
-  return out;
-}
-
-void test1() {
-  Graph g;
-  g.addEdge(5, 2);
-  g.addEdge(5, 0);
-  g.addEdge(4, 0);
-  g.addEdge(4, 1);
-  g.addEdge(2, 3);
-  g.addEdge(3, 1);
-
-  auto topOrder = topologicalSort(g);
-  auto roots = findRoots(g, topOrder);
-  std::cout << "top: " << topOrder << '\n';
-  std::cout << "roots: " << roots << '\n';
-}
-
-void test2() {
-  Graph g;
-  g.addEdge(5, 11);
-  g.addEdge(7, 11);
-  g.addEdge(7, 8);
-  g.addEdge(3, 8);
-  g.addEdge(3, 10);
-  g.addEdge(11, 2);
-  g.addEdge(11, 9);
-  g.addEdge(11, 10);
-  g.addEdge(8, 9);
-
-  auto topOrder = topologicalSort(g);
-  auto roots = findRoots(g, topOrder);
-
-  std::cout << "top: " << topOrder << '\n';
-  std::cout << "roots: " << roots << '\n';
-}
-
-void test3() {
-  Graph g;
-  g.addEdge(0, 1);
-  g.addEdge(0, 2);
-  g.addEdge(1, 3);
-  g.addEdge(1, 2);
-  g.addEdge(2, 4);
-  g.addEdge(2, 5);
-  g.addEdge(2, 3);
-  g.addEdge(3, 5);
-  g.addEdge(3, 4);
-  g.addEdge(4, 5);
-  g.addEdge(6, 2);
-  g.addEdge(6, 4);
-
-  auto topOrder = topologicalSort(g);
-  auto roots = findRoots(g, topOrder);
-
-  std::cout << "top: " << topOrder << '\n';
-  std::cout << "roots: " << roots << '\n';
-}
-
 int main() {
-  test1();
-  test2();
-  test3();
+  Graph g;
+  NodeIndex lastId = 0;
+  std::unordered_map<NodeIndex, std::string> nodeLabels;
+  std::unordered_map<std::string, NodeIndex> nodeIds;
+
+  auto getNodeId = [&](const auto &label) {
+    auto it = nodeIds.find(label);
+    if (it != end(nodeIds)) {
+      return it->second;
+    }
+
+    nodeIds.insert({label, lastId});
+    nodeLabels.insert({lastId, label});
+    return lastId++;
+  };
+
+  auto onEdge = [&](const auto &srcId, const auto &destId) {
+    const auto srcNodeId = getNodeId(srcId);
+    const auto destNodeId = getNodeId(destId);
+    g.addEdge(srcNodeId, destNodeId);
+  };
+  parseDotGraph(std::cin, onEdge);
+
+  std::cout << "node count: " << g.nodeCount() << '\n';
+
+  auto topOrder = topologicalSort(g);
+  auto roots = findRoots(g, topOrder);
+
+  auto toLabel = [&](NodeIndex i) { return nodeLabels.find(i)->second; };
+
+  std::cout << "=================\n"
+            << "topological order\n"
+            << "=================\n";
+
+  std::transform(begin(topOrder), end(topOrder),
+                 std::ostream_iterator<std::string>{std::cout, "\n"}, toLabel);
+  std::cout << '\n';
+
+  std::cout << "=====\n"
+            << "roots\n"
+            << "=====\n";
+
+  std::transform(begin(roots), end(roots),
+                 std::ostream_iterator<std::string>{std::cout, "\n"}, toLabel);
+  std::cout << '\n';
+
   return 0;
 }
