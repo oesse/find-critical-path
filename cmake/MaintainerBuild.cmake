a2ddebde726f934948d20bb6c8cebe891f4a04cd
Oliver Esser <oliver.esser@campus.tu-berlin.de>
2019-11-03 21:29:14 +0100

    Choose default build type based on context
    When in the context of a git repository the default is assumed to be
    Maintainer, otherwise Release.

--- cmake/MaintainerBuild.cmake
+++ cmake/MaintainerBuild.cmake
@@ -1,11 +1,19 @@
 set(CMAKE_CXX_FLAGS_MAINTAINER "${CMAKE_CXX_FLAGS_DEBUG} -Wall -Wextra")
 
-if(NOT CMAKE_BUILD_TYPE)
-  set(CMAKE_BUILD_TYPE "Maintainer")
+set(default_build_type "Release")
+if(EXISTS "${CMAKE_SOURCE_DIR}/.git")
+  set(default_build_type "Maintainer")
 endif()
 
-string(TOLOWER "${CMAKE_BUILD_TYPE}" build_type)
-if(build_type STREQUAL "maintainer")
-  message(STATUS "Maintainer build detected.")
-endif()
+if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
+  message(STATUS "Setting build type to '${default_build_type}' as none was specified.")
 
+  set(CMAKE_BUILD_TYPE "${default_build_type}"
+    CACHE STRING "Choose the type of build." FORCE)
+  # Set the possible values of build type for cmake-gui
+  set_property(CACHE CMAKE_BUILD_TYPE
+    PROPERTY STRINGS "Maintainer" "Debug" "Release" "MinSizeRel" "RelWithDebInfo")
+
+  set(ENABLE_CLANG_TIDY ON
+    CACHE BOOL "Enable static code analysis with clang-tidy" FORCE)
+endif()
