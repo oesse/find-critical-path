6d101eea8ddaf750a74b66ba35afc0d5445ede14
Oliver Esser <oliver.esser@campus.tu-berlin.de>
2019-11-03 20:01:19 +0100

    Use camelCase consistently for variable names

--- src/TopologicalSort.cpp
+++ src/TopologicalSort.cpp
@@ -8,23 +8,23 @@ namespace {
 
 auto countIncomingEdges(const Graph &g) -> std::vector<int> {
   const auto V = g.nodeCount();
-  std::vector<int> in_degree(V, 0);
+  std::vector<int> inDegree(V, 0);
 
   // Traverse adjacency lists to fill indegrees of
   // vertices.  This step takes O(V+E) time
   for (NodeIndex u = 0; u < V; u++) {
     for (auto adjacentIndex : g.adjacentNodes(u)) {
-      in_degree[adjacentIndex]++;
+      inDegree[adjacentIndex]++;
     }
   }
 
-  return in_degree;
+  return inDegree;
 }
 
 void enqueueRootNodes(std::queue<NodeIndex> &q,
-                      const std::vector<int> &in_degree) {
-  for (NodeIndex i = 0; i < in_degree.size(); i++) {
-    if (in_degree[i] == 0) {
+                      const std::vector<int> &inDegree) {
+  for (NodeIndex i = 0; i < inDegree.size(); i++) {
+    if (inDegree[i] == 0) {
       q.push(i);
     }
   }
@@ -35,17 +35,17 @@ void enqueueRootNodes(std::queue<NodeIndex> &q,
 auto topologicalSort(const Graph &g) -> NodeList {
   const auto V = g.nodeCount();
 
-  std::vector<int> in_degree = countIncomingEdges(g);
+  std::vector<int> inDegree = countIncomingEdges(g);
 
   // Create an queue and enqueue all vertices with
   // indegree 0
   std::queue<NodeIndex> q;
-  enqueueRootNodes(q, in_degree);
+  enqueueRootNodes(q, inDegree);
 
   // Initialize count of visited vertices
   std::size_t cnt = 0;
 
-  NodeList top_order;
+  NodeList topOrder;
 
   // One by one dequeue vertices from queue and enqueue
   // adjacents if indegree of adjacent becomes 0
@@ -54,7 +54,7 @@ auto topologicalSort(const Graph &g) -> NodeList {
     // and add it to topological order
     NodeIndex u = q.front();
     q.pop();
-    top_order.push_back(u);
+    topOrder.push_back(u);
 
     // Iterate through all its neighbouring nodes
     // of dequeued node u and decrease their in-degree
@@ -62,7 +62,7 @@ auto topologicalSort(const Graph &g) -> NodeList {
     for (auto adjacentIndex : g.adjacentNodes(u)) {
 
       // If in-degree becomes zero, add it to queue
-      if (--in_degree[adjacentIndex] == 0) {
+      if (--inDegree[adjacentIndex] == 0) {
         q.push(adjacentIndex);
       }
     }
@@ -72,5 +72,5 @@ auto topologicalSort(const Graph &g) -> NodeList {
 
   assert(cnt == V && "There must not be a cycle in the graph");
 
-  return top_order;
+  return topOrder;
 }
